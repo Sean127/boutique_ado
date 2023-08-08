dadfc26a53f04e7ac24192b3315f703b0da1c578
Sean127 <sdoran401@gmail.com>
2023-08-08 12:53:08 +0000

    Add size updating with the shopping bag

--- products/views.py
+++ products/views.py
@@ -9,7 +9,7 @@ from .models import Product, Category
 
 
 def all_products(request):
-    """  A view to show all products, including search and sorting queries."""
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -25,13 +25,13 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey = 'category_name'
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -40,14 +40,14 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-        current_sorting = f'{sort}_{direction}'
-    
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
@@ -59,7 +59,7 @@ def all_products(request):
 
 
 def product_details(request, product_id):
-    """  A view to show individual product details."""
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
